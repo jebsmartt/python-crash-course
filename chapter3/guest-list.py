f2bb0b3b584e88879f23b92c5991f44de335f35b
jebsmartt <jeb.smartt@gmail.com>
2023-06-21 19:37:05 -0700

    Complete TIY 3-5

--- chapter3/guest-list.py
+++ chapter3/guest-list.py
@@ -1,7 +1,11 @@
 guest_list = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Dumbledore','Snape','Voldemort']
 
+cant_attend = guest_list.pop(3)
+guest_list.append('Hagrid')
+
 def dinner_invitation(party):
     for i in party:
         print(f"{i}, would you like to come to dinner?")
 
 dinner_invitation(guest_list)
+print(f"\n{cant_attend} is unable to attend because {guest_list[3]} killed {cant_attend}")
