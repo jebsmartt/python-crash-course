f8e78dbe25fb9752749250d2a93c30c9e72a8681
jebsmartt <jeb.smartt@gmail.com>
2023-06-22 15:34:32 -0700

    Complete 5-1 thru 5-5 of TIY

---
+++ chapter5/alien_colors.py
@@ -0,0 +1,8 @@
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("The alien is green. You earned 5 points")
+elif alien_color == 'yellow':
+    print("The alien is yellow. You earned 10 points")
+else:
+    print("The alien was red. You earned 15 points")
