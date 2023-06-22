8209b79a42257ceb59ce86fafd98a94c6ecb5b8a
jebsmartt <jeb.smartt@gmail.com>
2023-06-22 15:15:53 -0700

    Complete Chapter 4

---
+++ chapter4/tuples.py
@@ -0,0 +1,10 @@
+# 4-13 Buffet
+
+menu = ('pizza', 'pasta','soup','steak','ice cream')
+
+# menu[3] = 'Vegemite'
+
+menu = ('hamburger', 'sushi','soup','steak','ice cream')
+
+for item in menu:
+    print(item)
