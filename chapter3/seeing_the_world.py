e48c232419a34f73d331edc53e63a688f613aa84
jebsmartt <jeb.smartt@gmail.com>
2023-06-21 21:53:55 -0700

    Complete TIY 3-8

---
+++ chapter3/seeing_the_world.py
@@ -0,0 +1,23 @@
+locations = ['Tokyo','Madison','Grindlewald','Islay','Yosemite']
+
+print(locations)
+
+print(sorted(locations))
+
+print(locations)
+
+print(sorted(locations, reverse=True))
+
+print(locations)
+
+locations.reverse()
+print(locations)
+
+locations.reverse()
+print(locations)
+
+locations.sort()
+print(locations)
+
+locations.sort(reverse=True)
+print(locations)
