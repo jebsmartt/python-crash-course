988dfcb89fbbed4c7c6e3915bbbef2712dbc21bb
jebsmartt <jeb.smartt@gmail.com>
2023-06-23 16:57:52 -0700

    Complete TIY 8-8

--- chapter8/album.py
+++ chapter8/album.py
@@ -13,6 +13,22 @@ def make_album(artist_name,album_title,number_songs=None):
         }
     return album
 
-print(make_album('Queen', 'Greatest Hits', 24))
-print(make_album('Earth Wind and Fire', 'September'))
-print(make_album('My Chemical Romance','Welcome to the Black Parade'))
+# print(make_album('Queen', 'Greatest Hits', 24))
+# print(make_album('Earth Wind and Fire', 'September'))
+# print(make_album('My Chemical Romance','Welcome to the Black Parade'))
+
+input_flag = True
+
+while input_flag == True:
+    f_artist = f"Who is your favorite artist? "
+    f_album = f"Which album of theirs is your favorite? "
+    
+    f_artist = input(f_artist)
+    f_album = input(f_album)
+
+    print(make_album(artist_name=f_artist,album_title=f_album))
+    ask_again = input(f"Do you have another favorite artist? (Y / N) ")
+
+    if ask_again != 'Y':
+        input_flag = False
+
