5b6f98c33e301c9df489c4fdc7f41b02902fb09b
jebsmartt <jeb.smartt@gmail.com>
2023-06-23 16:30:50 -0700

    Complete TIY 8-6

---
+++ chapter8/city_names.py
@@ -0,0 +1,6 @@
+def city_country(name, country):
+    return f"{name.title()}, {country.title()}"
+
+print(city_country('waco','United states'))
+print(city_country('vienna','austria'))
+print(city_country('London','England'))
