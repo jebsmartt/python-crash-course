33bc611b088853aa4a26ba9dd00a51e11e6a70c5
jebsmartt <jeb.smartt@gmail.com>
2023-06-23 14:20:06 -0700

    Complete TIY 7-9

--- chapter7/deli.py
+++ chapter7/deli.py
@@ -1,9 +1,16 @@
-sandwich_orders = ['Reuben','Hoagie','PB&J','Meatball Sub','Bahn Mi','Muffaleta']
+out_list = ['Pastrami']
+
+sandwich_orders = ['Reuben','Hoagie','PB&J','Meatball Sub','Pastrami','Bahn Mi','Muffaleta','Pastrami','Pastrami']
 
 finished_sandwiches = []
 
 while len(sandwich_orders) > 0:
     order = sandwich_orders.pop(0)
+    if order in out_list:
+        while order in sandwich_orders:
+            sandwich_orders.remove(order)
+        print(f"We are currently out of {order} sandwiches. Order something else.")
+        continue
     print(f"I made your {order} sandwich.")
     finished_sandwiches.append(order)
 
