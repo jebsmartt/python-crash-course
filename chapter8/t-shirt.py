30906a44bd5c4857796ef2237afb9d130d416b5c
jebsmartt <jeb.smartt@gmail.com>
2023-06-23 16:14:37 -0700

    Complete TIY 8-3 & 8-4

---
+++ chapter8/t-shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size ='Large', text='I Love Python'):
+    print(f"The shirt should be size {size} and have the text:")
+    print(f"\t{text}")
+
+make_shirt()
+make_shirt('Medium')
+make_shirt(size = 'Small', text="I am Tiny")
