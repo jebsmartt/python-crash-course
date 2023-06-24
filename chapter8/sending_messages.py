db9712f483827309e998cc3061d279535ec482ac
jebsmartt <jeb.smartt@gmail.com>
2023-06-23 23:16:40 -0700

    Complete TIY 8-10

---
+++ chapter8/sending_messages.py
@@ -0,0 +1,19 @@
+txt_msgs = ['Hello World!', 'Hey bby, what\'s up?', 'How is the weather there?', 'You here?']
+sent_messages = []
+
+# def print_text_messages(list):
+#     for i in list:
+#         print(i)
+
+# print_text_messages(txt_msgs)
+
+def send_messages(list):
+    while list:
+        message = list.pop()
+        print(message)
+        sent_messages.append(message)
+
+send_messages(txt_msgs)
+print(txt_msgs)
+print(sent_messages)
+        
