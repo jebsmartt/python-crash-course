653d2b50b953c0a8e50a90e22741662a6d07c528
jebsmartt <jeb.smartt@gmail.com>
2023-06-23 23:11:15 -0700

    Complete 8-9 TIY

---
+++ chapter8/messages.py
@@ -0,0 +1,7 @@
+txt_msgs = ['Hello World!', 'Hey bby, what\'s up?', 'How is the weather there?', 'You here?']
+
+def print_text_messages(list):
+    for i in list:
+        print(i)
+
+print_text_messages(txt_msgs)
