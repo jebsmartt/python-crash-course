6de34ebc6fd540d94f34bf35bb446747bdcece2b
jebsmartt <jeb.smartt@gmail.com>
2023-06-25 05:48:30 -0700

    Complete TIY 9-1

--- chapter9/restaurant.py
+++ chapter9/restaurant.py
@@ -1,3 +1,18 @@
 class restaurant:
     """this is called a docstring"""
 
+    def __init__(self, restaurant_name, cuisine_type) -> None:
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(self.restaurant_name)
+        print(self.cuisine_type)
+    
+    def open_restaurant(self):
+        print(f"The {self.restaurant_name} restaurant is open.")
+
+restaurant1 = restaurant("Luigi's",'Italian')
+
+restaurant.describe_restaurant(restaurant1)
+restaurant.open_restaurant(restaurant1)
