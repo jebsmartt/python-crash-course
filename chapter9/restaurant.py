3cbf3778d301132f9d6fc108c1a2f1809dc72440
jebsmartt <jeb.smartt@gmail.com>
2023-06-25 06:56:39 -0700

    Complete TIY 9-4

--- chapter9/restaurant.py
+++ chapter9/restaurant.py
@@ -4,6 +4,7 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type) -> None:
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(self.restaurant_name)
@@ -12,14 +13,18 @@ class Restaurant:
     def open_restaurant(self):
         print(f"The {self.restaurant_name} restaurant is open.")
 
+    def increment_number_served(self,number_of_customers):
+        self.number_served += number_of_customers
+
 restaurant1 = Restaurant("Luigi's",'Italian')
-
-Restaurant.describe_restaurant(restaurant1)
-
 restaurant2 = Restaurant("Paulies",'Pasta')
-
-Restaurant.describe_restaurant(restaurant2)
-
 restaurant3 = Restaurant("Chiloso",'Mexican')
 
 Restaurant.describe_restaurant(restaurant3)
+print(restaurant1.number_served)
+
+restaurant1.number_served = 14
+print(restaurant1.number_served)
+
+restaurant1.increment_number_served(12)
+print(restaurant1.number_served)
