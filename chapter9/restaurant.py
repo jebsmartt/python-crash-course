50159f1d4e6023a90185abbff1f8aec739fd8eaa
jebsmartt <jeb.smartt@gmail.com>
2023-06-26 10:10:27 -0700

    Complete TIY 9-6

--- chapter9/restaurant.py
+++ chapter9/restaurant.py
@@ -16,15 +16,30 @@ class Restaurant:
     def increment_number_served(self,number_of_customers):
         self.number_served += number_of_customers
 
-restaurant1 = Restaurant("Luigi's",'Italian')
-restaurant2 = Restaurant("Paulies",'Pasta')
-restaurant3 = Restaurant("Chiloso",'Mexican')
+# restaurant1 = Restaurant("Luigi's",'Italian')
+# restaurant2 = Restaurant("Paulies",'Pasta')
+# restaurant3 = Restaurant("Chiloso",'Mexican')
 
-Restaurant.describe_restaurant(restaurant3)
-print(restaurant1.number_served)
+# Restaurant.describe_restaurant(restaurant3)
+# print(restaurant1.number_served)
 
-restaurant1.number_served = 14
-print(restaurant1.number_served)
+# restaurant1.number_served = 14
+# print(restaurant1.number_served)
 
-restaurant1.increment_number_served(12)
-print(restaurant1.number_served)
+# restaurant1.increment_number_served(12)
+# print(restaurant1.number_served)
+
+class IceCreamStand(Restaurant):
+
+    def __init__(self, restaurant_name, cuisine_type="Ice Cream"):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = []
+
+    def show_flavors(self):
+        print(f"\nThese are our ice cream flavors today:")
+        for flavor in self.flavors:
+            print(f"- {flavor}")
+
+restaurant4 = IceCreamStand('Salt & Straw')
+restaurant4.flavors.extend(['Vanilla','Chocolate','Strawberry'])
+IceCreamStand.show_flavors(restaurant4)
