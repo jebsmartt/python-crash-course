0d21f336d4c2b6ff20e96de2183c036f4b5fc274
jebsmartt <jeb.smartt@gmail.com>
2023-06-27 20:18:06 -0700

    Follow along with Chapter 11 some more

--- chapter11/tiy_11_1/city_functions.py
+++ chapter11/tiy_11_1/city_functions.py
@@ -1,3 +1,6 @@
-def city_country(city,country):
-    formal_name = f"{city}, {country}"
-    return formal_name.title()
+def city_country(city, country, population = ''):
+    if population:
+        formal_name = f"{city.title()}, {country.title()} - population {population}"
+    else:
+        formal_name = f"{city.title()}, {country.title()}"
+    return formal_name
--- chapter11/tiy_11_1/test_cities.py
+++ chapter11/tiy_11_1/test_cities.py
@@ -4,3 +4,9 @@ def test_city_country():
     """Do cities like 'santiago' and 'chile' work?"""
     formatted_city_state = city_country('santiago','chile')
     assert formatted_city_state == 'Santiago, Chile'
+
+def test_city_country_population():
+    """Does the function handle passing in a 
+    population in additon to city and country"""
+    formatted_city_state_pop = city_country('santiago','chile',5_000_000)
+    assert formatted_city_state_pop == 'Santiago, Chile - population 5000000'
